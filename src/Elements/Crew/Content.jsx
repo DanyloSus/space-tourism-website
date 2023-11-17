8b642e72d99ffbf3b8cf1e7b4c21bd0db698c139
DanyloSus <danil.sushko19@gmail.com>
2023-11-17 18:59:49 +0200

    Crew content relative desktop update

--- src/Elements/Crew/Content.jsx
+++ src/Elements/Crew/Content.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import Slider from "react-slick";
 
 import "./slider.css";
@@ -31,6 +31,7 @@ class Content extends Component {
       initialSlide: 0,
       speed: 500,
       arrows: false,
+      lazyLoad: true,
       adaptiveHeight: true,
 
       dotsClass: "slick-dots slick-thumb",
@@ -39,39 +40,41 @@ class Content extends Component {
     const crew = data.crew;
 
     return (
-      <>
-        <h5 className="mr-auto ml-[165px] mt-[76px]">
-          <span className="opacity-[0.25] mr-[28px]">02</span>
-          Meet your crew
-        </h5>
-        <Slider {...settings} afterChange={this.afterChangeHandler}>
-          {crew.map((crewmate, index) => (
-            <div key={index}>
-              <div className="flex justify-center overflow-y-hidden h-content">
-                <div className="px-5 gap-5 max-w-[1138px] w-full flex justify-between">
-                  <div className="flex justify-center flex-col h-full">
-                    <div>
-                      <h4>{crewmate.role}</h4>
-                      <h3 className="mt-[15px] mb-[27px]">{crewmate.name}</h3>
-                      <p className="max-w-[444px] min-w-[222px]">
-                        {crewmate.bio}
-                      </p>
+      <div className="flex items-center justify-center">
+        <div className=" max-w-[1138px] w-full flex flex-col mt-[76px] overflow-x-visible">
+          <h5 className="px-5">
+            <span className="opacity-[0.25] mr-[28px]">02</span>
+            Meet your crew
+          </h5>
+          <Slider {...settings} afterChange={this.afterChangeHandler}>
+            {crew.map((crewmate, index) => (
+              <div key={index}>
+                <div className="max-w-[1138px] w-full flex justify-center h-[654px] mx-auto px-5">
+                  <div className=" gap-5 w-full flex justify-between">
+                    <div className="flex justify-center flex-col h-full z-10">
+                      <div>
+                        <h4>{crewmate.role}</h4>
+                        <h3 className="mt-[15px] mb-[27px]">{crewmate.name}</h3>
+                        <p className="max-w-[444px] min-w-[222px]">
+                          {crewmate.bio}
+                        </p>
+                      </div>
                     </div>
+                    <img
+                      src={crewmate.images.png}
+                      alt={`Photo of ${crewmate.name}`}
+                      className={
+                        (index === 1 ? "-scale-x-100" : "") +
+                        " h-full max-w-[568px] max-h-[712px] object-cover mt-auto"
+                      }
+                    />
                   </div>
-                  <img
-                    src={crewmate.images.png}
-                    alt={`Photo of ${crewmate.name}`}
-                    className={
-                      (index === 1 ? "-scale-x-100" : "") +
-                      " h-full max-w-[568px] max-h-[712px] object-cover mt-auto"
-                    }
-                  />
                 </div>
               </div>
-            </div>
-          ))}
-        </Slider>
-      </>
+            ))}
+          </Slider>
+        </div>
+      </div>
     );
   }
 }
