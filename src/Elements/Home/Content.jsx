4ccb4027fa6323da9d58429e2df0dba39db27f2a
DanyloSus <danil.sushko19@gmail.com>
2023-11-19 14:08:29 +0200

    Animation for home page

--- src/Elements/Home/Content.jsx
+++ src/Elements/Home/Content.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import Explore from "./Explore";
 
+import classes from "./animations.module.css";
+
 class Content extends Component {
   render() {
     return (
       <div className="flex items-end justify-center md:hidden sm:hidden">
         <div className="max-w-[1110px] px-5 w-full flex  items-end justify-between h-content pb-[131px]">
           <div className="w-[444px]">
-            <h5>So, you want to travel to</h5>
-            <h1 className="my-6">Space</h1>
-            <p>
+            <h5 className={classes.left}>So, you want to travel to</h5>
+            <h1 className={classes.right + " my-6"}>Space</h1>
+            <p className={classes.left}>
               Let’s face it; if you want to go to space, you might as well
               genuinely go to outer space and not hover kind of on the edge of
               it. Well sit back, and relax because we’ll give you a truly out of
--- src/Elements/Home/MobContent.jsx
+++ src/Elements/Home/MobContent.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import Explore from "./Explore";
 
+import classes from "./animations.module.css";
+
 class MobContent extends Component {
   render() {
     return (
       <div className="sm:flex hidden text-center justify-between mt-12 flex-col items-center h-max pb-12">
         <div className="max-w-[327px] w-full px-5 mb-20">
-          <span className="sub2 text-[16px]">SO, YOU WANT TO TRAVEL TO</span>
-          <h2 className="text-[80px] my-4">SPACE</h2>
-          <p>
+          <span className={classes.left + " sub2 text-[16px]"}>
+            SO, YOU WANT TO TRAVEL TO
+          </span>
+          <h2 className={classes.right + " text-[80px] my-4"}>SPACE</h2>
+          <p className={classes.left}>
             Let’s face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we’ll give you a truly out of this
